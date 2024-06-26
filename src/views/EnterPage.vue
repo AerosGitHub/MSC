f3396b1b5673b0b9681982ec835c19f6ade662f9
Александр Чумаченко <AC.Sasha20041994@gmail.com>
2024-06-26 18:55:01 +0300

    first commit

---
+++ src/views/EnterPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div class="headerGroup">
+    <img src="../assets/img/STSpic.jpg" alt="Sts logo">
+    <h1>Студенческий совет <br/> МИЭТ</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: white;
+  -webkit-text-stroke: 2px black;
+}
+</style>
